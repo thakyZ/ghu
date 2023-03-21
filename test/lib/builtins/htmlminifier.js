1567019954d0bca656f23900f29a114677a7763c
thakyZ <thakyz@outlook.com>
2023-03-21 09:51:16 -0700

    Fixed minifier

--- test/lib/builtins/htmlminifier.js
+++ test/lib/builtins/htmlminifier.js
@@ -3,39 +3,33 @@ const {htmlminifier} = require('../../../lib/ghu');
 
 test('lib.htmlminifier()', () => {
     assert.equal(typeof htmlminifier, 'function');
-    assert.equal(typeof htmlminifier(), 'function');
+    assert.equal(typeof htmlminifier(), 'object');
 });
 
-test('lib.htmlminifier() - no objects', () => {
-    const fn = htmlminifier();
+test('lib.htmlminifier() - no objects', async () => {
     const objs = [];
 
-    return fn(objs).then(async val => {
-        console.log({soup2: await val});
-        assert.deep_equal(await val, []);
-    });
+    const val = await htmlminifier(objs);
+    console.log({soup4: val});
+    assert.deep_equal(val, []);
 });
 
-test('lib.htmlminifier() - empty', () => {
-    const fn = htmlminifier();
+test('lib.htmlminifier() - empty', async () => {
     const content = '';
     const expected = '';
     const objs = [{source: 'a.html', content}];
 
-    return fn(objs).then(async val => {
-        console.log({soup2: await val});
-        assert.deep_equal(await val, [{source: 'a.html', content: expected}]);
-    });
+    const val = await htmlminifier(objs);
+    console.log({soup4: val});
+    assert.deep_equal(val, [{source: 'a.html', content: expected}]);
 });
 
-test('lib.htmlminifier() - whitespace', () => {
-    const fn = htmlminifier();
+test('lib.htmlminifier() - whitespace', async () => {
     const content = '<p  >   </p >';
-    const expected = '<p>   </p>';
+    const expected = '<p> </p>';
     const objs = [{source: 'a.html', content}];
 
-    return fn(objs).then(async val => {
-        console.log({soup2: await val});
-        assert.deep_equal(await val, [{source: 'a.html', content: expected}]);
-    });
+    const val = await htmlminifier(objs);
+    console.log({soup4: val});
+    assert.deep_equal(val, [{source: 'a.html', content: expected}]);
 });
